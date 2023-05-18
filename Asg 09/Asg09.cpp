84f6e0e27c844188739186ccfec3bed05ef12f17
SOham <114175483+soham-19@users.noreply.github.com>
2023-05-18 12:09:41 +0530

    Update Asg09.cpp

--- Asg 09/Asg09.cpp
+++ Asg 09/Asg09.cpp
@@ -16,7 +16,7 @@ class Node {
     Node(string key, string value) {
         keyword = key;
         meaning = value;
-        ht = 1;
+        ht = 0;
         left = NULL;
         right = NULL;
     }
@@ -34,34 +34,44 @@ class AVL {
 	int BF(Node *t);
 	void inOrder(Node *t);
 	Node *rotateRight(Node *t);
+	Node *rotateLeft(Node *t);
+	Node* LL(Node* t);
+	Node* RR(Node* t);
+	Node* LR(Node* t);
+	Node* RL(Node* t);
+	Node* insert(Node*,string,string);
 };
 
 int AVL::height(Node*t){
 	if(t==NULL){
 		return 0;
-	}
+	} /*else if(t->left==NULL && t->right==NULL)
+		return 0;*/
 	return max(height(t->left), height(t->right)) + 1;
 }
 
 int AVL::BF(Node*t){
+/*
 	if(t==NULL){
 		return 0;
 	}
 	int lh, rh;
 
 	if(t->left==NULL){
-		lh = 0;
+		lh = 1;
 	} else {
 		lh = t->left->ht + 1;
 	}
 
 	if(t->right==NULL){
-		rh = 0;
+		rh = 1;
 	} else {
 		rh = t->right->ht + 1;
 	}
-
-	return lh - rh;
+	cout << "Curr bf = " <<lh - rh << endl;*/
+	/*return lh - rh;*/
+	cout << "Curr bf = " <<height(t->left)-height(t->right) << endl;
+	return height(t->left)-height(t->right);
 }
 
 void AVL::inOrder(Node*root){
@@ -86,16 +96,99 @@ Node* AVL::rotateRight(Node*x){
 	return y;
 }
 
+Node* AVL::rotateLeft(Node*x){
+
+	Node * y;
+
+	y= x->right;
+	x->right = y->left;
+	y->left = x;
+
+	x->ht = height(x);
+	y->ht = height(y);
+
+	return y;
+}
+
+Node* AVL::LL(Node* t){
+
+	cout << "LL Case\n";
+	t = rotateRight(t);
+	return t;
+}
+
+Node* AVL::RR(Node* t){
+
+	cout << "RR Case\n";
+	t = rotateLeft(t);
+	return t;
+}
+
+Node* AVL::LR(Node* t){
+
+	cout << "LR Case\n";
+	t->left = rotateLeft(t->left);
+	t = rotateRight(t);
+	return t;
+}
+
+Node* AVL::RL(Node* t){
+
+	cout << "RL Case\n";
+	t->right = rotateRight(t->right);
+	t = rotateLeft(t);
+	return t;
+}
+
+Node* AVL::insert(Node* root, string key, string mean){
+
+	Node* curr = NULL;
+	int lh,rh;
+	
+	if(root==NULL){
+			root = new Node(key,mean);
+			return root;
+	}
+	else {
+	
+		if(key < root->keyword){
+			root->left = insert(root->left,key,mean);
+			cout << "BF : " << root->keyword << " : " << BF(root) << endl;
+			
+			if(BF(root)==2){
+				if(key < root->left->keyword){
+					root = LL(root);
+				} else {
+					root = LR(root);
+				}
+			}
+		} else if (key > root->keyword){
+			root->right = insert(root->right,key,mean);
+			cout << "BF : " << root->keyword << " : " << BF(root) << endl;
+			
+			if(BF(root)==-2){
+				if(key < root->right->keyword){
+					root = RR(root);
+				} else {
+					root = RL(root);
+				}
+			}
+		}
+	}
+}
 int main(){
 
 	AVL *t = new AVL();
-	t->root = new Node("soham", "SOH");
-	t->root->right = new Node("xebra", "SOH");
-	t->root->left = new Node("flag", "FLAG");
-	t->root->left->left = new Node("apple", "fruit");
-
 	t->inOrder(t->root);
-	cout << endl;
-	t->root = t->rotateRight(t->root);
-	t->inOrder(t->root);
+	for(int i=0; i<5; i++){
+		cout << "Enter word : ";
+		string s;
+		cin >> s;
+		string m;
+		cout << "Enter meaning : ";
+		cin >> m;
+		
+		t->root = t->insert(t->root,s,m);
+		t->inOrder(t->root);
+	}
 }
